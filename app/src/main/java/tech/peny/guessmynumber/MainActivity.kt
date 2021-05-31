9c2341472e538d3bbff4712708022db6dddc6160
Prince Nyamadi <princenyamadi.pn@gmail.com>
2021-05-31 17:32:59 +0000

    Initial Commit

---
+++ app/src/main/java/tech/peny/guessmynumber/MainActivity.kt
@@ -0,0 +1,11 @@
+package tech.peny.guessmynumber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
